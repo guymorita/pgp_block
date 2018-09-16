1582830ef5c83bd9b3c5f33059ba6337b20ef749
Guy Morita <guymorita@gmail.com>
2018-09-16 15:44:37 -0400

    Add encryptMessage

--- src/components/PGP.jsx
+++ src/components/PGP.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import openpgp from 'openpgp'
+import * as openpgp from 'openpgp'
 
 import { Button, Panel } from 'react-bootstrap'
 
@@ -9,7 +9,10 @@ export default class PGP extends Component {
     this.state = {
       privateKey: null,
       publicKey: null,
-      revocationSignature: null
+      revocationSignature: null,
+      encryptedMessage: null,
+      messageToEncrypt: "",
+      passphrase: "Big things have small beginnings"
     }
   }
 
@@ -28,8 +31,33 @@ export default class PGP extends Component {
     });
   }
 
+  updateMessage(e) {
+    this.setState({ messageToEncrypt: e.target.value })
+  }
+
+  encryptMessage() {
+    const { messageToEncrypt, publicKey, passphrase } = this.state
+    openpgp.key.readArmored(publicKey).then((keys) => {
+      return keys
+    }).then((keys) => {
+      const options = {
+        message: openpgp.message.fromText(messageToEncrypt),
+        publicKeys: keys.keys
+      }
+      openpgp.encrypt(options).then((ciphertext) => {
+        this.setState({
+          encryptedMessage: ciphertext.data
+        })
+      })
+    })
+  }
   render() {
     const { privateKey, publicKey } = this.state
+    const {
+      messageToEncrypt,
+      encryptedMessage,
+      privateKey,
+      publicKey } = this.state
     return (
       <div>
         <div>
@@ -49,8 +77,31 @@ export default class PGP extends Component {
             <Panel.Body>{privateKey}</Panel.Body>
           </Panel>
         </div>
-        <h1>Encrypt Message</h1>
         <h1>Decrypt Message</h1>
+        <div>
+          <h4>
+            Encrypt Message <label><Button
+              bsStyle="primary"
+              bsSize="small"
+              onClick={this.encryptMessage.bind(this)}
+            >Encrypt</Button></label>
+          </h4>
+          <Panel>
+            <Panel.Heading>Message to Encrypt</Panel.Heading>
+            <Panel.Body>
+              <FormControl
+                componentClass="textarea"
+                value={messageToEncrypt}
+                placeholder="Enter text to encrypt"
+                onChange={this.updateMessage.bind(this)}
+              />
+            </Panel.Body>
+          </Panel>
+          <Panel>
+            <Panel.Heading>Encrypted Message</Panel.Heading>
+            <Panel.Body>{encryptedMessage}</Panel.Body>
+          </Panel>
+        </div>
       </div>
     )
   }
